07fc3733d7e0e505ae4d067670e2e3f914e92309
Berta Castillo Crespo <897199@unizar.es>
2026-09-19 20:57:51 +0200

    fix zeros BANK7 and BANK9

--- p1/ATM Cobol_backup/BANK7.cbl
+++ p1/ATM Cobol_backup/BANK7.cbl
@@ -89,7 +89,7 @@
            88 UP-ARROW-PRESSED      VALUE 2003.
            88 DOWN-ARROW-PRESSED    VALUE 2004.
            88 ESC-PRESSED           VALUE 2005.
-       77 PRESSED-KEY               PIC   9(4).
+       77 PRESSED-KEY               PIC   X.
 
        77 USER-NUM-ENTRADAS         PIC   9(2).
        77 USER-NUM-ESPECT           PIC   9(4).
